ba19f54e1f9107f0c6073531e6b24d9703dc9561
bikram bhattarai <bikrambhattarai296@gmail.com>
2023-12-18 19:52:16 +0545

    up:

--- app.js
+++ app.js
@@ -1,11 +1,61 @@
 const express = require("express");
-const app=express();
+const app = express();
 require("./model/index");
+require("dotenv").config();
+const passport = require("passport");
 
-app.get('/',(req,res)=>{
-    res.send('hello')
-})
-const PORT= process.env.PORT || 4000
-app.listen(PORT,()=>{
-   console.log(`"port started at"${PORT}`) 
-})
+//require ends here
+
+//middleware
+// app.use every rreequest ma execute huncha
+app.set("view engine","ejs")
+app.use(passport.initialize()); // passport use garna lako vanne command yo
+app.use(passport.session());
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user); //cb(error,success) --> cb(error)
+});
+passport.deserializeUser(function (obj, cb) {
+  //desereliazer ma object huncha
+  cb(null, obj);
+});
+
+//db connection
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+//google login
+var userProfile;
+let GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+passport.use(new GoogleStrategy({
+    clientID:process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL:"http://localhost:3000/auth/google/auth"
+
+},
+
+function(accessToken,refreshToken,profile,done){
+    userProfile=profile;
+
+    return done(null,userProfile)  // error or succes vo vane user profile pass garcha
+    // teo data gayera serializer ma bascha
+}
+));
+app.get("/auth/google",passport.authenticate("google",{scope :['profile','email']}))
+app.get('/auth/google/callback',passport.authenticate("google",{
+    failureRedirect:'http://localhost:3000'
+}),
+function(req,res){
+    res.send("logged in successfully")
+
+}
+);
+
+
+
+// emd of google login
+
+app.listen(3000, () => {
+  console.log("NodeJs project has started at port 3000");
+});
