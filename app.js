bd34b14143e64e22ec1e84cd8f3e8616d396d6f8
bikram bhattarai <bikrambhattarai296@gmail.com>
2023-12-27 21:15:48 +0545

    oauth

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const passport = require("passport");
 
 //middleware
 // app.use every rreequest ma execute huncha
-app.set("view engine","ejs")
+app.set("view engine", "ejs");
 app.use(passport.initialize()); // passport use garna lako vanne command yo
 app.use(passport.session());
 
@@ -19,41 +19,39 @@ passport.deserializeUser(function (obj, cb) {
   //desereliazer ma object huncha
   cb(null, obj);
 });
-
 //db connection
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
-
 //google login
 var userProfile;
 let GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
-passport.use(new GoogleStrategy({
-    clientID:process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL:"http://localhost:3000/auth/google/auth"
-
-},
-
-function(accessToken,refreshToken,profile,done){
-    userProfile=profile;
-
-    return done(null,userProfile)  // error or succes vo vane user profile pass garcha
-    // teo data gayera serializer ma bascha
-}
-));
-app.get("/auth/google",passport.authenticate("google",{scope :['profile','email']}))
-app.get('/auth/google/callback',passport.authenticate("google",{
-    failureRedirect:'http://localhost:3000'
-}),
-function(req,res){
-    res.send("logged in successfully")
-
-}
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      userProfile = profile;
+      return done(null, userProfile); // error or succes vo vane user profile pass garcha
+      // teo data gayera serializer ma bascha
+    }
+  )
 );
-
-
-
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] }));
+app.get("/auth/google/callback",passport.authenticate("google", {
+    failureRedirect: "http://localhost:3000",
+  }),
+  function (req, res) {
+    console.log(userProfile);
+    res.send("logged in successfully");
+    console.log("hello wrodl")
+  }
+);
 // emd of google login
 
 app.listen(3000, () => {
